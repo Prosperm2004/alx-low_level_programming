d6b97543bd571a71399b5ddab9d5bd833d0fd180
prosperm2004 <promic2004@gmail.com>
2023-02-21 11:38:32 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints 10 times
+ * the alphabet, in lowercase
+ * Return: always 0;
+ */
+
+void print_alphabet_x10(void)
+{
+	int n, i;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
