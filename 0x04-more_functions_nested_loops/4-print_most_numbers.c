30179d649f52dc6319ad5889614e7c13c48a87ca
prosperm2004 <promic2004@gmail.com>
2023-02-23 05:20:58 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ * Description - You can only use _putchar twice in your code
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+			a++;
+		}
+	}
+	_putchar('\n');
+}
