7fcc1a3421dc9812dd7d78fbde6ddfed58d32241
prosperm2004 <promic2004@gmail.com>
2023-02-27 11:42:05 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: int type pointer
+ * @b: int type pointer
+ * Return: nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
