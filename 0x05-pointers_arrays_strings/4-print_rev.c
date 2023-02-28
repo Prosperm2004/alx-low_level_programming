60a17fc838cbaf69b749ae5e9f580b5c38bd1f99
prosperm2004 <promic2004@gmail.com>
2023-02-27 16:33:48 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (i--; i >= 0; i--)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
