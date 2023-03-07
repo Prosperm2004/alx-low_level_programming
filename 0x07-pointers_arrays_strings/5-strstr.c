214fae1ac2b92954537ce1b8026c1fb456793ac5
prosperm2004 <promic2004@gmail.com>
2023-03-06 18:06:38 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strstr - locate a substring
+ * @haystack: string
+ * @needle: string substring
+ * Return: Pointer to beginning of substring, or NULL if not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *p = needle;
+
+		while (*i == *p && *p != '\0')
+		{
+			i++;
+			p++;
+		}
+
+		if (*p == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
