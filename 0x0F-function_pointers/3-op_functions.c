759faa47e0969a9e79d18f871c1da14645617d5c
prosperm2004 <promic2004@gmail.com>
2023-03-23 06:18:58 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - addition operator 
+ * @a: first int
+ * @b: second int
+ * Return:  returns the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction operator
+ * @a: first int
+ * @b: second int
+ * Return: returns the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication operator
+ * @a: first int
+ * @b: second int
+ * Return: returns the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division operator
+ * @a: first int
+ * @b: second int
+ * Return: returns the result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - get the remainder of the division
+ * @a: first int
+ * @b: second int
+ * Return: returns the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
