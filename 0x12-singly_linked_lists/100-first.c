24967009759695dccc129bf904d8316039b73f0d
Prosperm2004 <promic2004@gmail.com>
2023-03-31 09:05:46 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void myStartupFun (void) __attribute__ ((constructor));
+void myStartupFun(void) __attribute__ ((constructor));
 
 /**
  * myStartupFun - fuction that is executed before main
  */
 
-void myStartupFun (void)
+void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
