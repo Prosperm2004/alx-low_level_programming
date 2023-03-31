6ca0b4f62e9cdd2e7d8d7d9ff3fc445391cf1cbf
Prosperm2004 <promic2004@gmail.com>
2023-03-31 08:07:51 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,6 +16,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+#include <stdlib.h>
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
