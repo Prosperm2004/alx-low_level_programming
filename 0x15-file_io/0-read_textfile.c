8a972a562cd06ddfcd4e761d1a7c4b703eaaf944
Prosperm2004 <promic2004@gmail.com>
2023-04-10 13:44:22 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,49 @@
 #include "main.h"
 
 /**
- * read_textfile - Read a text file and print to POSIX stdout
- * @filename: char string of files name
- * @letters: number of letters to read and print
- * Return: number of letters read and printed, or 0 if error
+ * read_textfile - Reads a file and prints it to the POSIX stdout.
+ * @filename: The name of the file that needs to be read.
+ * @letters: Is the number of letters the function should print.
+ * Return: The actual number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t rcount, wcount;
-	char *buffer;
+	int rd;
+	int i;
+	int reading;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
-
-	fd = open("filename", O_RDWR);
-	if (fd == -1)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	rd = open(filename, O_RDONLY);
+	if (rd == -1)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
 
-	rcount = read(fd, buffer, letters);
-	if (rcount == -1)
+	reading = read(rd, buf, letters);
+	if (reading == -1)
+	{
+		close(rd);
+		free(buf);
 		return (0);
+	}
 
-	wcount = write(fd, buffer, rcount);
-	if (wcount == 1 || rcount != wcount)
-		return (0);
-	free (buffer);
-	close(fd);
+	for (i = 0; i < reading; i++)
+	{
+		if (write(STDOUT_FILENO, &buf[i], 1) == -1)
+		{
+			close(rd);
+			free(buf);
+			return (0);
+		}
 
-	return (wcount);
+	}
+	close(rd);
+	free(buf);
+	return (reading);
 }
